9a021aa0f827f127885fa60f9588d304b1d7cbe7
killo3967 <killo3967@gmail.com>
2023-07-19 19:15:46 +0200

    Corregido que en cadenas del tipo "Bob. 's" se tome el "." como un separador entre frases. Corregido que se pierdan los caracteres CR y LF al pasar por Google

--- translate_summary_field.py
+++ translate_summary_field.py
@@ -22,16 +22,30 @@ clr.AddReference('System.Web')
 from System.Web import HttpUtility
 
 import json 
+import time
+import re
 
 # Setting Variables
 global debug, target_lang, separator_chars
 debug = True
+
+# Define staticaly the target lang
 target_lang = ""
-separator_chars = ['.', '?', '!']
+
+# Define the separator chars 
+separator_chars = ['.', '?', '!', '\n']
+# Define the exceptions
+no_separator_chars_pattern1 = r"(\w+\.\s?'\w)"
+# Define illegal chars (to replace by space)
+illegal_chars = ['<', '>']
+
+# Time delays (in seconds)
+delay_between_sentences = 0.15
+delay_between_comics = 1
 
 # Get locale data to obtain the target language.
 import locale
-# if not staticaly defined
+# If not staticaly defined the source language y try to get it.
 if target_lang == "":
     target_lang = str(locale.getdefaultlocale()).strip("()'").split(',',1)[0].split('_',1)[0]
 
@@ -55,15 +69,23 @@ def translate_summary_field(books):
     # Main Loop
     for book in books:
         if debug: print("")
-        if debug: print(">> Processing comic: " + unicode(book.FilePath))
+        if debug: print(">> Processing comic: " + unicode(book.FilePath))   # unicode = str
         v_summary = ""
         v_summary = (book.Summary).strip()
+
         # If Summary has data
         if v_summary:
             if debug: 
                 print("Summary for translate: " + v_summary)
                 print ("  ----------")
-            # Split the summary into individual sentences
+            
+            # Replace the "." in certain patterns like "Bob. 's", before splitting.
+            matches = re.findall(no_separator_chars_pattern1, v_summary)
+            if matches:
+                for match in matches:
+                    v_summary = v_summary.replace(match, match.replace(".", ""))
+
+            # Split the summary into individual sentences for better translations (by Google).
             sentences = []
             sentence = ""
             for char in v_summary:
@@ -74,26 +96,54 @@ def translate_summary_field(books):
                     elif char == '?':
                         sentences.append('?' + sentence)
                     elif char == '!':
-                        sentences.append('?' + sentence)
+                        sentences.append('!' + sentence)
+                    elif char == '\n':
+                        sentences.append('\n' + sentence)
                     sentence = ""
                 else:
                     sentence += char
+
+                # Time delay between sencences
+                # time.sleep(delay_between_sentences)
+            
             if sentence:
                 sentences.append(sentence)
         
             # Translate each sentence and join them back together
             translated_sentences = []
             for sentence in sentences:
+                
+                # Chech CR/LF at start and at end before translations and before cleaning
+                start_chars=''
+                end_chars=''
+                if sentence.startswith('\n\r') or sentence.startswith('\r\n'):
+                    start_chars = '\r\n'
+                if sentence.endswith('\n\r') or sentence.endswith('\r\n'):
+                    end_chars = '\r\n'
+                if sentence.startswith('\n'):
+                    start_chars = '\r\n'
+                if sentence.endswith('\n'):
+                    end_chars = '\r\n'
+                
+                # Now i could clean and translate
                 c_sentence = sentence_clean(sentence)
                 sentence=c_sentence
                 if sentence:
                     if debug: print("   >>Sentence for translate: " + sentence)
-                    # NOW Translate
+                    # NOW Translate without non ascii characters
                     translated_sentence = google_translate_text(sentence)
+
+                    # And now, after translate, recover the CRLF
+                    if start_chars is not None or end_chars is not None:
+                        translated_sentence = start_chars + translated_sentence + end_chars
                     if debug: 
                         print("   >>Translated sentence: " + translated_sentence)
                         print ("  ----------")
-                    translated_sentence = translated_sentence.rstrip('\r\n')
+                    # translated_sentence = translated_sentence.rstrip('\r\n')
+                    
+                    # Add delay time
+                    # time.sleep(delay_between_sentences)
+                    
                     # Add the output to the list
                     translated_sentences.append(translated_sentence)
         
@@ -103,7 +153,9 @@ def translate_summary_field(books):
             book.Summary = translated_text
             # Add a custom value for information, and for create smart list.
             book.SetCustomValue("Summary_translated",target_lang)
-            # print (">> Comic: " + unicode(book.FilePath) + " field 'SUMMARY' Translated <<")    
+            
+            # Add a delay time between comic books
+            # time.sleep(delay_between_comics)
         else:
             if debug: print("Summary field empty, nothing to traslate.")
     print("")
